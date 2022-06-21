34f084d4a5a7274225fd08cd1253c0805050bf8b
Ian Hall <ianmhall6@gmail.com>
2022-06-21 16:52:26 -0400

    A lot of behind the scenes work done in JS.  Daily goals completed.  Realized I needed to get at least some of the CSS done to be able to tell if the js work I'd put in was functioning so there was some work done on the boxes specifically.

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
-
+let prompt = [];
+let playerPrompt = [];
+let step = 0;
 let startBtn = document.getElementById('btnStart');
-startBtn.addEventListener("click", startGame)
+const wirePanel = document.querySelector('.wire-panel');
+const info = document.querySelector('.game-info');
+const startInfo = document.querySelector('.start-info');
+const startDiv = document.querySelector('.startDiv');
+// const warningLabel = document.querySelector('.backgroundInfo');
+function activateBox(color) {
+  const box = document.querySelector(`[data-box='${color}']`);
+  const sound = document.querySelector(`[data-sound='${color}']`);
+  box.classList.add('activated');
+  sound.play();
+  setTimeout(() =>{
+    box.classList.remove('activated');
+  }, 300);
+}
+
+function playRound(nextPrompt) {
+  nextPrompt.forEach((color, index) => {
+    setTimeout(() => {
+      activateBox(color);
+    }, (index + 1) * 600);
+  });
+}
+
+function nextStep() {
+  const boxes = ['red', 'green', 'blue', 'yellow'];
+  const random = boxes[Math.floor(Math.random() * boxes.length)];
+  return random;
+}
+
+function nextRound() {
+  step += 1;
+  
+  const nextPrompt = [...prompt];
+  nextPrompt.push(nextStep());
+  playRound(nextPrompt);
+}
 
 function startGame() {
+  startBtn.classList.add('hidden');
+  wirePanel.classList.remove('hidden');
+  startInfo.classList.add('hidden');
+  info.classList.remove('hidden');
+  // startDiv.classList.add('hidden')
+  
+  // warningLabel.classList.add('hidden');
+  info.textContent = 'WAIT FOR THE PROMPT';
+  nextRound();
   let minute = 4;
   let sec = 59;
   setInterval(function() {
@@ -10,8 +56,8 @@ function startGame() {
     sec--;
     if (sec == 0 && minute > 0) {
       minute --;
-    sec = 59;
-    if (minute == 0 && sec == 0) {
+      sec = 59;
+      if (minute == 0 && sec == 0) {
         boom();
       }
     }
@@ -19,11 +65,10 @@ function startGame() {
       sec = `0${sec}`;
     }
   }, 1000);
-  }
+}
 
 
-// function computeTwoDigitNumber(value) {
-//   if (value > 9) { 
-// return value.toString()
-//  } else return '0' + value
-// };
+startBtn.addEventListener("click", startGame)
+
+
+
