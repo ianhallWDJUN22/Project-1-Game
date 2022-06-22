0228ba8753316f0771951faa142feda8d71b74cb
Ian Hall <ianmhall6@gmail.com>
2022-06-22 10:55:46 -0400

    Morning update.  Got a good chunk of the js written and functioning.  Wanted to save that as it took me a considerable amount of debugging to get to this point.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let prompt = [];
 let playerPrompt = [];
 let step = 0;
 let startBtn = document.getElementById('btnStart');
-const wirePanel = document.querySelector('.wire-panel');
 const info = document.querySelector('.game-info');
+const wirePanel = document.querySelector('.wire-panel');
 const startInfo = document.querySelector('.start-info');
 const startDiv = document.querySelector('.startDiv');
 const warningLabel = document.querySelector('.backgroundInfo');
@@ -11,9 +11,10 @@ const warningLabel = document.querySelector('.backgroundInfo');
 
 function activateBox(color) {
   const box = document.querySelector(`[data-box='${color}']`);
-  const sound = document.querySelector(`[data-sound='${color}']`);
+  // const sound = document.querySelector(`[data-sound='${color}']`);
   box.classList.add('activated');
-  sound.play();
+  // sound.play();
+  
   setTimeout(() =>{
     box.classList.remove('activated');
   }, 300);
@@ -27,6 +28,11 @@ function playRound(nextPrompt) {
   });
 }
 
+function playerTurn(step) {
+  wirePanel.classList.remove('unclickable');
+  info.textContent = `INPUT CODE FOR SEQUENCE.. (${step})`;
+}
+
 function nextStep() {
   const boxes = ['red', 'green', 'blue', 'yellow'];
   const random = boxes[Math.floor(Math.random() * boxes.length)];
@@ -36,9 +42,26 @@ function nextStep() {
 function nextRound() {
   step += 1;
   
+  wirePanel.classList.add('unclickable');
+  info.textContent = '...PLAYING REFERENCE CODE...'
+  // This would be the spot to add a leve/step/round tracker 
+  // notification should I add one.  Just remember to create 
+  //a variable for it then (variable.textContent)
+
   const nextPrompt = [...prompt];
   nextPrompt.push(nextStep());
   playRound(nextPrompt);
+  
+  prompt = [...nextPrompt];
+  setTimeout(() => {
+    playerTurn(step);
+  }, step * 600 + 1000);
+}
+
+function playerClick(box) {
+  const arrayIndex = playerPrompt.push(box) - 1;
+  // const sound = document.querySelector(`[data-sound='${box}']`);
+  // sound.play();
 }
 
 function startGame() {
@@ -47,7 +70,8 @@ function startGame() {
   startInfo.classList.add('hidden');
   info.classList.remove('hidden');
   warningLabel.classList.add('hidden');
-  info.innerHTML = '...PLAYING CODE REFERENCE...';
+  info.textContent = '...PLAYING REFERENCE CODE...';
+  nextRound();
   let minute = 4;
   let sec = 59;
   setInterval(function() {
@@ -64,22 +88,15 @@ function startGame() {
       sec = `0${sec}`;
     }
   }, 1000);
-  nextRound();
 }
-
-// .blink_me {
-//   animation: blinker 1s linear infinite;
-// }
-
-// @keyframes blinker {
-//   50% {
-//     opacity: 0;
-//   }
-// }
-
 
 
 startBtn.addEventListener("click", startGame)
+wirePanel.addEventListener('click', event => {
+  const { box } = event.target.dataset;
+
+  if (box) playerClick(box);
+})
 
 
 
